09d159964d13a947c61299c6a6c79bfa299e2575
noverd <71782778+noverd@users.noreply.github.com>
2020-10-13 18:57:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python
 # -- coding: utf-8 -
-
 import discord
-import json
-import requests
-from discord.ext import commands
 
-TOKEN = 'xx'
+bot = discord.Client()
+mat = ["хуй","пидор","сука","нахуй"]
+@bot.event
+async def on_message(message):
+    mes = message.content
+    user = message.author
+    arg = "Асужденный"
+    role_mat = discord.utils.get(message.guild.roles, name=str(arg))
+    if mes.lower() in mat:
+        await user.add_roles(str(role_mat))
 
-# Префикс комынды боту. В данном случае это '!'
-bot = commands.Bot(command_prefix='!')
 
-# Функция test
-@bot.command()
-async def test(ctx, argu):  # создаем асинхронную фунцию test, читаем аргумент arg
-    await ctx.send(argu)  # отправляем обратно аргумент
-
-# Функция hello
-@bot.command()
-async def hello(ctx):  # Создаём ассинхронную функцию hello
-    author = ctx.message.author  # Объявляем переменную author и записываем туда информацию об авторе запроса боту.
-    await ctx.send('Hello, {}!'.format(author))  # Отпраляем ответ Hello с обращением к автору.
-
-# Функция отображения случайных картинок rim
-@bot.command()
-async def rim(ctx, arg_im):  # создаем асинхронную фунцию rim, читаем аргумент arg_im
-    response = requests.get('https://some-random-api.ml/img/{}'.format(arg_im))  # Запрос картинки
-    json_data = json.loads(response.text)  # Извлекаем JSON
-
-    embed = discord.Embed(color=0x72BAB6, title='Random image with {}'.format(arg_im))  # Создание оформления
-    embed.set_image(url=json_data['link'])  # Устанавливаем картинку
-    await ctx.send(embed=embed)  # Отправляем
-
-bot.run(TOKEN)  # запускаем бота
+bot.run("NzE0NzQyNDY3OTI0MTk3NDc2.XszFyw.P7paOTijc2qerB7yelDWGpHnWy8")
