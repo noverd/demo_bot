15a9e690b30b5bf8d22fff39d8f94e44246b9f44
noverd <71782778+noverd@users.noreply.github.com>
2020-10-17 17:30:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,1 @@
-#!/usr/bin/python
-# -- coding: utf-8 -
-import discord
 
-bot = discord.Client()
-mat = ["хуй","пидор","сука","нахуй"]
-@bot.event
-async def on_message(message):
-    mes = message.content
-    user = message.author
-    arg = "Асужденный"
-    role_mat = discord.utils.get(message.guild.roles, name=str(arg))
-    if mes.lower() in mat:
-        await user.add_roles(str(role_mat))
-
-
-bot.run("NzE0NzQyNDY3OTI0MTk3NDc2.XszFyw.P7paOTijc2qerB7yelDWGpHnWy8")
